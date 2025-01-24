d917c25604dcbc449e3d3da67ad9c295eb013506
Piyush Tiwari <tiwaripiyush89555@gmail.com>
2025-01-24 15:30:50 +0530

    adwance react

--- Projects/2-bharat-clock-version-one/src/components/CurrentTime.jsx
+++ Projects/2-bharat-clock-version-one/src/components/CurrentTime.jsx
@@ -1,6 +1,25 @@
-function CurrentTime(){
+import { useEffect } from "react";
+import { useState } from "react";
 
-    let time = new Date();
-    return <p>This is the Current Time : {time.toLocaleDateString()} - {""}{time.toLocaleTimeString()}</p>
+function CurrentTime(){
+  const [time,setTime] =   useState(new Date());
+
+  useEffect(() => {
+       console.log("Interval has been setup");
+       const inetervalId = setInterval(()=>{
+        setTime(new Date());
+       },1000);
+
+       return () => {
+        clearInterval(inetervalId);
+        console.log("cancelled the interval");
+       }
+  },[]);
+    return (
+      <p>
+        This is the Current Time : {time.toLocaleDateString()} - {""}
+        {time.toLocaleTimeString()}
+      </p>
+    ); 
 }
 export default CurrentTime;
